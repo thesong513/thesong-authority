5bb14ffc06e1dee070459901beb3f0738e4506db
thesong513 <30562225+thesong513@users.noreply.github.com>
2020-11-16 17:38:28 +0800

    add .

---
+++ authority-auth/src/main/java/com/thesong/authority/AuthorityApplication.java
@@ -0,0 +1,18 @@
+package com.thesong.authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author thesong
+ * @Date 2020/11/16 17:36
+ * @Version 1.0
+ * @Describe
+ */
+
+@SpringBootApplication
+public class AuthorityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityApplication.class,args);
+    }
+}
